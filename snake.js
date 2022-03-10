63ca05c9885fa04090c70c4c79a181c1b09e4987
MohammadYSF <aa.yosefiyan7@gmail.com>
2022-03-10 13:55:28 +0330

    Creating and adding Snake object in snake.js file

---
+++ snake.js
@@ -0,0 +1,15 @@
+function Snake() {
+    this.x = 100;
+    this.y = 100;
+    this.xspead = 1;
+    this.yspead = 0;
+    this.squareSize = 10;
+    this.update = () => {
+        this.x += this.xspead;
+        this.y += this.yspead;
+    }
+    this.show = (ctx) => {
+        ctx.fillStyle = "#eee";
+        ctx.fillRect(this.x, this.y, this.squareSize, this.squareSize);
+    }
+}
