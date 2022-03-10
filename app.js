15486fbcd2f7bc4a24dc5419c1eb2c3112377724
MohammadYSF <aa.yosefiyan7@gmail.com>
2022-03-10 13:56:47 +0330

    setting the app.js to work with snake object and some canvas setups

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const ctx = canvas.getContext("2d");
 const resize = () =>{
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    console.log(window.innerWidth);
+    
 };
 window.addEventListener('load' , () => {
     resize();
     
 });
+
 window.addEventListener('resize' , resize);
+let snake = new Snake();
+snake.show(ctx);
+console.log(snake);
+const animate = () => {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    snake.show(ctx);
+    snake.update();
+}
+animate();
