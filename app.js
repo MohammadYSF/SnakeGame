d2e6887e02a1870d6e46be098fedd3951b967b6b
MohammadYSF <aa.yosefiyan7@gmail.com>
2022-03-11 23:22:23 +0330

    not being able the snake to go backwards featured added

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const frames_per_second = 10;
 const squareSize = 20;
 const canvas_width = window.innerWidth - 900;
 const canvas_height = window.innerHeight - 200;
+let last_key_code_pressed = 39;
 let food = {
     x: 0,
     y: 0
 };
 let columns = Math.floor((canvas_width - squareSize) / squareSize);
-let rows = Math.floor((canvas_height-squareSize) / squareSize);
-const getRandomLocation = () =>{
+let rows = Math.floor((canvas_height - squareSize) / squareSize);
+const getRandomLocation = () => {
     let random_row = Math.floor((Math.random() * rows) + 1);
     let random_column = Math.floor((Math.random() * columns) + 1);
-    let pos = new Position(random_column * squareSize , random_row * squareSize);
+    let pos = new Position(random_column * squareSize, random_row * squareSize);
     return pos;
 }
 
@@ -22,7 +23,7 @@ const pickLocation = () => {
     let random_column = Math.floor((Math.random() * columns) + 1);
     food.x = getRandomLocation().x;
     food.y = getRandomLocation().y;
-    
+
 }
 const resize = () => {
     canvas.width = canvas_width;
@@ -57,30 +58,42 @@ const animate = () => {
 }
 animate();
 window.addEventListener('keydown', (e) => {
-   
+
     //up
     if (e.keyCode == 38) {
-
-        snake.dir(0, -1);
-        e.preventDefault();
+        if (last_key_code_pressed != 40) {
+            snake.dir(0, -1);
+            e.preventDefault();
+            last_key_code_pressed = 38;
+        }
     }
     //down
     else if (e.keyCode == 40) {
-        snake.dir(0, 1);
-        e.preventDefault();
+        if (last_key_code_pressed != 38) {
+            snake.dir(0, 1);
+            e.preventDefault();
+            last_key_code_pressed = 40;
+        }
 
     }
     //left
     else if (e.keyCode == 37) {
-        snake.dir(-1, 0);
-        e.preventDefault();
+        if (last_key_code_pressed != 39) {
+            snake.dir(-1, 0);
+            e.preventDefault();
+            last_key_code_pressed = 37;
+        }
 
     }
     //right
     else if (e.keyCode == 39) {
-        snake.dir(1, 0);
-        e.preventDefault();
-    
+        if (last_key_code_pressed != 37) {
+            snake.dir(1, 0);
+            e.preventDefault();
+            last_key_code_pressed = 39;
+        }
+
+
     }
 });
 
